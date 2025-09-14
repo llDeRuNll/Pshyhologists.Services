a7aa0ca42524db387d92b4cb6159253709f9f5b9
llDeRuNll <eduardprudiys@gmail.com>
2025-09-14 18:49:29 +0200

    added main folders

---
+++ pshyhologists.service/src/pages/RegestrationPage/RegestrationPage.tsx
@@ -0,0 +1,5 @@
+const RegestrationPage = () => {
+  return <div>RegestrationPage</div>;
+};
+
+export default RegestrationPage;
---
+++ pshyhologists.service/src/routes/PrivateRoute/PrivateRoute.tsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from "../../redux/auth/selectors";
+
+const PrivateRoute = ({ children }) => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  if (isRefreshing) return null;
+
+  return isLoggedIn ? children : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
